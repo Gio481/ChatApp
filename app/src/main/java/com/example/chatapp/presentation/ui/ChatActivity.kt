08fa25aa44c9013e1bcfc0b2f1f3f388238154ce
Giorgi <Gio.beridze33@gmail.com>
2022-04-09 21:00:12 +0400

    create chat activity, with two fragment container views in its layout

--- app/src/main/java/com/example/chatapp/presentation/ui/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/presentation/ui/ChatActivity.kt
@@ -1,11 +1,12 @@
-package com.example.chatapp
+package com.example.chatapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.R
 
-class MainActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chat)
     }
 }
