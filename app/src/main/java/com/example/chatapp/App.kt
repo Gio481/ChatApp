e832ffe426289972624352ee8f1343080dd9d8c5
Giorgi <Gio.beridze33@gmail.com>
2022-04-11 13:23:31 +0400

    create di module of chat database

---
+++ app/src/main/java/com/example/chatapp/App.kt
@@ -0,0 +1,16 @@
+package com.example.chatapp
+
+import android.app.Application
+import com.example.chatapp.di.databaseModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(databaseModule))
+        }
+    }
+}
