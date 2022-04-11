538f26a3817da429274b280d857c9b0ebd93627b
Giorgi <Gio.beridze33@gmail.com>
2022-04-11 13:16:56 +0400

    create chat entity

---
+++ app/src/main/java/com/example/chatapp/data/datasource/local/entity/ChatEntity.kt
@@ -0,0 +1,11 @@
+package com.example.chatapp.data.datasource.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "chat")
+data class ChatEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long? = null,
+    val message:String,
+    val time:String,
+)
