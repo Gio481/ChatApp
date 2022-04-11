cd0767c5760619d6d19630f0bc750a45268129cc
Giorgi <Gio.beridze33@gmail.com>
2022-04-12 00:33:55 +0400

    added returned type of abstract fun in database

--- app/src/main/java/com/example/chatapp/data/datasource/local/ChatDatabase.kt
+++ app/src/main/java/com/example/chatapp/data/datasource/local/ChatDatabase.kt
@@ -2,9 +2,10 @@ package com.example.chatapp.data.datasource.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.chatapp.data.datasource.local.dao.ChatDao
 import com.example.chatapp.data.datasource.local.entity.ChatEntity
 
 @Database(entities = [ChatEntity::class], version = 1)
 abstract class ChatDatabase : RoomDatabase() {
-    abstract fun chatDao()
+    abstract fun chatDao(): ChatDao
 }
