a454891ace8237c231750dc78458fdabe1c8fee0
Giorgi <Gio.beridze33@gmail.com>
2022-04-12 16:32:29 +0400

    change list adapter with recycler view's adapter

--- app/src/main/java/com/example/chatapp/presentation/ui/chat/adapter/ChatAdapter.kt
+++ app/src/main/java/com/example/chatapp/presentation/ui/chat/adapter/ChatAdapter.kt
@@ -2,15 +2,15 @@ package com.example.chatapp.presentation.ui.chat.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapp.databinding.MessageReceiverItemLayoutBinding
 import com.example.chatapp.databinding.MessageSenderItemLayoutBinding
 import com.example.chatapp.domain.model.ChatDomain
-import com.example.chatapp.util.ItemsDIffUtil
 
-class ChatAdapter(private val user: String) :
-    ListAdapter<ChatDomain, RecyclerView.ViewHolder>(ItemsDIffUtil()) {
+class ChatAdapter(private val user: String) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val itemList: MutableList<ChatDomain> = mutableListOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return if (viewType == MESSAGE_SENDER) {
             MessageSenderViewHolder(MessageSenderItemLayoutBinding.inflate(
@@ -21,8 +21,12 @@ class ChatAdapter(private val user: String) :
         }
     }
 
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
     override fun getItemViewType(position: Int): Int {
-        return if (user == getItem(position).user) {
+        return if (user == itemList[position].user) {
             MESSAGE_SENDER
         } else {
             MESSAGE_RECEIVER
@@ -31,8 +35,8 @@ class ChatAdapter(private val user: String) :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is MessageSenderViewHolder -> holder.onBind(getItem(position))
-            is MessageReceiverViewHolder -> holder.onBind(getItem(position))
+            is MessageSenderViewHolder -> holder.onBind(itemList[position])
+            is MessageReceiverViewHolder -> holder.onBind(itemList[position])
         }
     }
 
@@ -56,8 +60,20 @@ class ChatAdapter(private val user: String) :
         }
     }
 
+    fun setData(newList: List<ChatDomain>) {
+        this.itemList.clear()
+        this.itemList.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+    fun addSingleMessage(chatDomain: ChatDomain) {
+        this.itemList.add(chatDomain)
+        notifyItemInserted(itemList.size - 1)
+    }
+
     companion object {
         private const val MESSAGE_SENDER = 0
         private const val MESSAGE_RECEIVER = 1
     }
+
 }
