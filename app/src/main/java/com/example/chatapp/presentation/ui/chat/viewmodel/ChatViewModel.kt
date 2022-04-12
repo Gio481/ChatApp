061334288aaee8c3e2e4e82f16dba209b7f55415
Giorgi <Gio.beridze33@gmail.com>
2022-04-12 16:39:29 +0400

    configure chat viewmodel

--- app/src/main/java/com/example/chatapp/presentation/ui/chat/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/example/chatapp/presentation/ui/chat/viewmodel/ChatViewModel.kt
@@ -1,6 +1,53 @@
 package com.example.chatapp.presentation.ui.chat.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatapp.R
+import com.example.chatapp.domain.model.ChatDomain
+import com.example.chatapp.domain.usecase.GetMessagesUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ChatViewModel:ViewModel() {
+class ChatViewModel(private val useCase: GetMessagesUseCase) : ViewModel() {
+
+    private val _chatLiveData: MutableLiveData<List<ChatDomain>> = MutableLiveData()
+    val chatLiveData: LiveData<List<ChatDomain>> = _chatLiveData
+
+    private val _errorLiveData: MutableLiveData<Int> = MutableLiveData()
+    val errorLiveData: LiveData<Int> = _errorLiveData
+
+    fun getAllMessages() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _chatLiveData.postValue(useCase.getAllMessages())
+        }
+    }
+
+    fun sendMessage(user: String, message: String, action: (message: ChatDomain) -> Unit) {
+        if (message.isNotBlank()) {
+            val userMessage = ChatDomain(user = user, message = message, time = getTime())
+            action(userMessage)
+        }
+    }
+
+    fun insertMessage(user: String, message: String) {
+        viewModelScope.launch {
+            if (message.isNotBlank()) {
+                useCase.insertMessage(ChatDomain(user = user, message = message, time = getTime()))
+            } else {
+                _errorLiveData.postValue(R.string.blank_message_error_text)
+            }
+        }
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getTime(): String {
+        val sdf = SimpleDateFormat("MMM dd, HH:mm")
+        return sdf.format(Calendar.getInstance().time)
+    }
 }
