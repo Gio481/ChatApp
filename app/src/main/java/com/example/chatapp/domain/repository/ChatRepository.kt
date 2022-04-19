4f45134b5c2a294bfb07b4c6344e3f97e66a1239
Giorgi <Gio.beridze33@gmail.com>
2022-04-19 23:32:42 +0400

    fetch dao's function which displays time of last item

--- app/src/main/java/com/example/chatapp/domain/repository/ChatRepository.kt
+++ app/src/main/java/com/example/chatapp/domain/repository/ChatRepository.kt
@@ -5,4 +5,5 @@ import com.example.chatapp.domain.model.ChatDomain
 interface ChatRepository {
     suspend fun getAllMessages(): List<ChatDomain>
     suspend fun insertMessage(message: ChatDomain)
+    suspend fun getTime(): Long?
 }
